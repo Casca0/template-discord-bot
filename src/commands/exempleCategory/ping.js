2724ed9fce145c289507714df02dceb70cccbf10
Lucas Borzani <66306188+Casca0@users.noreply.github.com>
2023-05-10 16:37:24 -0300

    Fixed typo

--- src/commands/exempleCategory/ping.js
+++ src/commands/exempleCategory/ping.js
@@ -5,7 +5,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replys with PONG!')
+		.setDescription('Replies with PONG!')
 		.setDMPermission(false),
 	async execute(interaction) {
 		return interaction.followUp('PONG!');
